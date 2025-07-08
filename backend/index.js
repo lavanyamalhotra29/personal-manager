0566b6ff46fb3e6aab1a5b9f562195e19a2d527b
Lavanya Malhota <lavanyaamalhotra29@gmail.com>
2025-07-08 22:36:08 +0530

    backend setup with npm

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Backend basic setup!');
-});
+app.get("/",function(req,res){
+    res.send('backend working');
+})
 
 app.listen(3000, function(){
     console.log("backend is working");
